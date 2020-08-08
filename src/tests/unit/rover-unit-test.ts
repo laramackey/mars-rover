76dec8148d87c14b242fbc19d71925f73c177e29
laramackey <lara.mackey@focusrite.com>
2020-08-08 14:05:12 +0100

    Refactor rover tests for individual move & turn

--- src/tests/unit/rover-unit-test.ts
+++ src/tests/unit/rover-unit-test.ts
@@ -12,51 +12,56 @@ test('it stays in the same position if passed an empty string', (assert) => {
   assert.end();
 });
 
-test('it moves forward in one direction when passed only FFF in command', (assert) => {
-  const northRover = new Rover(0, 0, 'N');
-  const northRoverPosition = northRover.command('FFF');
-  assert.equal(
-    northRoverPosition,
-    '0 3 N',
-    'y value should increase by 3 for North rover'
-  );
-
-  const eastRover = new Rover(3, 7, 'E');
-  const eastRoverPosition = eastRover.command('FFF');
-  assert.equal(
-    eastRoverPosition,
-    '6 7 E',
-    'x value should increase by 3 for East rover'
-  );
-
-  const southRover = new Rover(2, 5, 'S');
-  const southRoverPosition = southRover.command('FFF');
-  assert.equal(
-    southRoverPosition,
-    '2 2 S',
-    'y value should decrease by 3 for South rover'
-  );
-
-  const westRover = new Rover(3, 5, 'W');
-  const westRoverPosition = westRover.command('FFF');
-  assert.equal(
-    westRoverPosition,
-    '0 5 W',
-    'x value decrease increase by 3 for West rover'
-  );
-  assert.end();
+[
+  {direction: 'N', endPos: [5, 8]},
+  {direction: 'E', endPos: [8, 5]},
+  {direction: 'S', endPos: [5, 2]},
+  {direction: 'W', endPos: [2, 5]},
+].forEach((testCase) => {
+  test(`${testCase.direction} facing rover moves forward in one direction`, (assert) => {
+    const testRover = new Rover(5, 5, testCase.direction);
+    const endPosition = testRover.command('FFF');
+    assert.equal(
+      endPosition,
+      `${testCase.endPos[0]} ${testCase.endPos[1]} ${testCase.direction}`,
+      `end position should be ${testCase.endPos}`
+    );
+    assert.end();
+  });
 });
 
-test('it turns right', (assert) => {
-  const testRover = new Rover(0, 0, 'W');
-  const endPosition = testRover.command('R');
-  assert.equal(endPosition, '0 0 N', 'end direction should be North');
-  assert.end();
+[
+  {oldDirection: 'N', newDirection: 'E'},
+  {oldDirection: 'E', newDirection: 'S'},
+  {oldDirection: 'S', newDirection: 'W'},
+  {oldDirection: 'W', newDirection: 'N'},
+].forEach((testCase) => {
+  test(`it turns right from ${testCase.oldDirection} to ${testCase.newDirection}`, (assert) => {
+    const testRover = new Rover(4, 5, testCase.oldDirection);
+    const endPosition = testRover.command('R');
+    assert.equal(
+      endPosition,
+      `4 5 ${testCase.newDirection}`,
+      `end direction should be ${testCase.newDirection}`
+    );
+    assert.end();
+  });
 });
 
-test('it turns left', (assert) => {
-  const testRover = new Rover(0, 0, 'N');
-  const endPosition = testRover.command('L');
-  assert.equal(endPosition, '0 0 W', 'end direction should be West');
-  assert.end();
+[
+  {oldDirection: 'N', newDirection: 'W'},
+  {oldDirection: 'E', newDirection: 'N'},
+  {oldDirection: 'S', newDirection: 'E'},
+  {oldDirection: 'W', newDirection: 'S'},
+].forEach((testCase) => {
+  test(`it turns left from ${testCase.oldDirection} to ${testCase.newDirection}`, (assert) => {
+    const testRover = new Rover(4, 5, testCase.oldDirection);
+    const endPosition = testRover.command('L');
+    assert.equal(
+      endPosition,
+      `4 5 ${testCase.newDirection}`,
+      `end direction should be ${testCase.newDirection}`
+    );
+    assert.end();
+  });
 });
